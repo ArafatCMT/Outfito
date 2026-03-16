4693793d568e41d9be9d5590a873c515152cdb49
Arafat <arafatemon6969@gmail.com>
2026-03-16 10:18:58 +0600

    create shop page

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const nav = document.getElementById('navbar');
 if(bar){
     bar.addEventListener("click" , ()=>{
         nav.classList.add("active");
+        document.body.classList.add('overlay');
     })
 };
 if(close){
     close.addEventListener("click" , ()=>{
         nav.classList.remove("active");
+        document.body.classList.remove('overlay');
     })
 };
